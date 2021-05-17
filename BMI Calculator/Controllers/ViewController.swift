d4c22451c1d8926f1c187f65a3a9c8738001d220
JedediahAlax <jedediah@me.com>
2021-05-16 20:21:17 -0600

    added code to pass bmiValue from first VC to second.

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSliderLabel: UISlider!
     
+    @IBOutlet weak var weightSliderLabel: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -32,6 +34,21 @@ class ViewController: UIViewController {
         weightLabel.text = "\(weight)Kg"
     }
     
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        let height = heightSliderLabel.value
+        let weight = weightSliderLabel.value
+        
+        let bmi = weight / pow(height, 2)
+        
+        // Create object for second viewController
+        let secondVC = SecondViewController()
+        
+        // assign the bmi to the second viewControllers bmiValue property, format to string because bmiValue is a string data type
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        // present the second viewController
+        self.present(secondVC, animated: true, completion: nil)
+    }
 }
 
 
